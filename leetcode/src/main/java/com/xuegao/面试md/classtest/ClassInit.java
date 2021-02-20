1109cb96a1ef1d26db8d78f12114b78fde979f36
WarriorFromLongAgo <hubeixuegao@gmail.com>
2021-02-20 14:25:08 +0800

    thread

---
+++ leetcode/src/main/java/com/xuegao/面试md/classtest/ClassInit.java
@@ -0,0 +1,20 @@
+package com.xuegao.面试md.classtest;
+
+/**
+ * <br/> @PackageName：com.xuegao.面试md.classtest
+ * <br/> @ClassName：ClassInit
+ * <br/> @Description：
+ * <br/> @author：xuegao
+ * <br/> @date：2021/02/20 11:16
+ */
+public class ClassInit {
+    // 1.首先初始化父类的static变量和块，按出现顺序
+   // 2.初始化子类的static变量和块，按出现顺序
+   // 3.初始化父类的普通变量，调用父类的构造函数
+   // 4.初始化子类的普通变量，调用子类的构造函数
+
+}
+
+class Father{
+
+}
